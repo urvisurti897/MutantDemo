ada40a39489ddbb4fcdccd1c0e67b49a0d6dc8ed
urvisurti897 <urvisurti897@gmail.com>
2025-02-03 12:22:46 -0500

    Add project files.

---
+++ IsoscelesTest/IsoscelesTest.cs
@@ -0,0 +1,44 @@
+﻿using MutantDemo;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace IsoscelesTest
+{
+    [TestFixture]
+    public class IsoscelesTest
+    {
+        [Test]
+        public void IsoscelesTriangle_InputTwoSideEqual_OutputIsoscelesTriangle()
+        {
+            //Arrange
+            int firstSide = 4;
+            int secondSide = 4;
+            int thirdSide = 8;
+
+            string expected = "The triangle is isosceles.";
+
+            //Act
+            string actual = Triangle.IsoscelesTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+                
+        [Test]
+        public void IsoscelesTriangle_InputThreeSideUnEqual_OutputNotIsoscelesTriangle()
+        {
+            //Arrange
+            int firstSide = 4;
+            int secondSide = 5;
+            int thirdSide = 6;
+
+            string expected = "The triangle is isosceles.";
+
+            //Act
+            string actual = Triangle.IsoscelesTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using MutantDemo;
+
+namespace MutantDemo
+{
+    public static class Triangle
+    {
+        public static string IsoscelesTriangle(int firstSide, int secondSide, int thirdSide)
+        {
+            string result;
+            if ((firstSide == secondSide) || (secondSide == thirdSide) || (thirdSide == firstSide))
+            {
+                result = "The triangle is isosceles.";
+            }
+            else
+            {
+                result = "The triangle is NOT isosceles.";
+            }
+            return result;
+        }
+    }
+
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstSide = 4;
+            int secondSide = 4;
+            int thirdSide = 6;
+
+            string result = Triangle.IsoscelesTriangle(firstSide, secondSide, thirdSide);
+            Console.WriteLine(result);
+        }
+    }
+}
+
